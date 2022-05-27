0b21e6abde119b199b789166d6799d4e1d236200
DiegoQuieroGomez <diego.quiero.g90@gmail.com>
2022-05-27 15:55:14 -0400

    Update objetos.js

--- app/objetos.js
+++ app/objetos.js
@@ -1,19 +1,80 @@
-class Cuadro{
-    constructor(categoria, genero, medida, color, precio){
-    this.categoria =categoria
-    this.genero = genero
-    this.medida = medida
-    this.color = color
-    this.precio = precio
+
+//Clases
+class ConsolaVideoJuego{
+        constructor(nombre, fabricante, modelo, color, precio){
+        this.nombre = nombre
+        this.fabricante = fabricante
+        this.modelo = modelo
+        this.color = color
+        this.precio = precio
     }
 }
 
-const cuadro1 = new Cuadro("Recien Nacido","Niño","Pequeño", "Celeste", 35000)
-const cuadro2 = new Cuadro("Recien Nacido","Niño","Mediano", "Celeste", 45000)
-const cuadro3 = new Cuadro("Recien Nacido","Niño","Grande", "Celeste", 55000)
-const cuadro4 = new Cuadro("Recien Nacido","Niña","Pequeño", "Rosa", 35000)
-const cuadro5 = new Cuadro("Recien Nacido","Niña","Mediano", "Rosa", 45000)
-const cuadro6 = new Cuadro("Recien Nacido","Niña","Grande", "Rosa", 55000)
+class VideoJuego{
+       constructor(categoria, titulo,ConsolaVideoJuego,precio){
+        this.categoria = categoria
+        this.titulo = titulo
+        this.ConsolaVideoJuego = ConsolaVideoJuego
+        this.precio = precio
+       }
+}
+
+//Objetos
+
+//Consolas de Video Juegos
+
+//PSX ----->
+
+//PS5 MODELO CFI-1100B
+const consola1a = new ConsolaVideoJuego("Play Station 5", "Sony","CFI-1100B", "Blanca",300000)
+const consola1b = new ConsolaVideoJuego("Play Station 5","Sony","CFI-1100B", "Negra",300000)
+//PS5 MODELO CFI-11015B
+const consola1c = new ConsolaVideoJuego("Play Station 5", "Sony","CFI-11015B", "Blanca",350000)
+const consola1d = new ConsolaVideoJuego("Play Station 5", "Sony","CFI-11015B", "Negra",350000)
+//PS5 MODELO FFVII ESPECIAL EDITION
+const consola1e = new ConsolaVideoJuego("Play Station 4", "Sony","CFI-1100B", "FFVII ESPECIAL EDITION",500000)
+
+
+//PS4 MODELO Slim 
+const consola2a = new ConsolaVideoJuego("Play Station 4", "Sony","Slim", "Blanca",200000)
+//PS4 MODELO PRO
+const consola2b = new ConsolaVideoJuego("Play Station 4", "Sony","PRO", "Negra",250000)
+
+
+//XBOX----->
+
+//XBOX MODELO S
+const consola3a = new ConsolaVideoJuego("XBOX Series", "Microsoft","S", "Negra",350000)
+const consola3b = new ConsolaVideoJuego("XBOX Series", "Microsoft","S", "Blanca",350000)
+//XBOX MODELO X
+const consola3c = new ConsolaVideoJuego("XBOX Series", "Microsoft","X", "Negra",450000)
+const consola3D = new ConsolaVideoJuego("XBOX Series", "Microsoft","X", "Blanca",450000)
+
+
+//XBOX ONE
+const consola4a = new ConsolaVideoJuego("XBOX ONE","Microsoft", "Classic", "Verde",200000)
+const consola4b = new ConsolaVideoJuego("XBOX ONE","Microsoft", "Classic", "Azul",200000)
+//XBOX ONE MODELO S
+const consola4c = new ConsolaVideoJuego("XBOX ONE","Microsoft", "S", "Roja",200000)
+
+
+//NINTENDO----->
+
+//Nintendo Oled
+const consola5a = new ConsolaVideoJuego("Nintendo Switch","Nintendo","Oled", "Blanca", 450000)
+const consola5b = new ConsolaVideoJuego("Nintendo Switch","Nintendo","Oled", "Negra", 450000)
+//Nintendo Switch classic
+const consola5c = new ConsolaVideoJuego("Nintendo Switch","Nintendo","Classic", "Blanca", 350000)
+const consola5d = new ConsolaVideoJuego("Nintendo Switch","Nintendo","Classic", "Azul", 350000)
+
+//Nintendo DS XL
+const consola5e = new ConsolaVideoJuego("Nintendo DS","Nintendo","XL", "Silver", 150000)
+//Nintendo DS
+const consola5f = new ConsolaVideoJuego("Nintendo DS","Nintendo","Classic", "Gold", 15000)
+
+
+
+
 
 /*
 const mostrarPorConsola = (cuadro) =>{
