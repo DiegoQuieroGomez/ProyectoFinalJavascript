9d0d8bb470e905cae967e12abef9011252281984
Diegoquiero <61029293+Diegoquiero@users.noreply.github.com>
2022-06-02 00:09:13 -0400

    Update
    Se arreglan objetos y metodos.

--- app/objetos.js
+++ app/objetos.js
@@ -2,18 +2,19 @@
 //Clases
 
 class Fabricante{
-    constructor(nombre, marca){
+    constructor(nombre, marca,producto){
         this.nombre = nombre
         this.marca = marca
+        this.producto = producto
+        
 
     }
 }
 
 class Consola{
-    constructor(Fabricante,modelo, marca, color, precio, imagen){
+    constructor(Fabricante,modelo, color, precio, imagen){
         this.Fabricante = Fabricante
         this.modelo = modelo
-        this.marca = marca
         this.color = color
         this.precio = precio
         this.imagen = imagen
@@ -23,10 +24,10 @@ class Consola{
 }
 
 class VideoJuego{
-       constructor(titulo, categoria, Consola,precio, imagen){
+       constructor(titulo, categoria, Fabricante,precio, imagen){
         this.titulo = titulo
         this.categoria = categoria
-        this.Consola = Consola
+        this.Fabricante = Fabricante
         this.precio = precio
         this.imagem = imagen
        }
@@ -38,152 +39,153 @@ class VideoJuego{
 
 //Fabricante ----->
 
-const playStation = new Fabricante("Sony","PlayStation")
-const xbox = new Fabricante("Microsoft","Xbox")
-const nintendo = new Fabricante("Nintendo","Nintendo")
+const playStation5 = new Fabricante("Sony","playStation", "PS5")
+const playStation4 = new Fabricante("Sony","playStation","PS4")
+const xboxSeries = new Fabricante("Microsoft","xbox", "XBOX Series")
+const xboxOne = new Fabricante("Microsoft","xbox","Xbox One")
+const nintendoSwitch = new Fabricante("Nintendo Company","nintendo","Switch")
+const nintendoDS = new Fabricante("Nintendo Company","nintendo","DS")
 
 
 //Consola ------>
-const consola1a = new Consola = (playStation,"CFI-1100B","PS5","Blanco",300000)
-const consola1b =
+//PS5
+const consola1a = new Consola(playStation5,"cfi-1100b","blanco",300000,"")
+const consola1b = new Consola(playStation5,"cfi-1100b","negro",300000,"")
+const consola1c = new Consola(playStation5,"cfi-11015b","blanco",300000,"")
+const consola1d = new Consola(playStation5,"cfi-11015b","negro",300000,"")
 
-//PSX ----->
-/*
-//PS5 MODELO CFI-1100B
-const consola1a = new ConsolaVideoJuego("Play Station 5", "Sony","CFI-1100B", "Blanca",300000,"")
-const consola1b = new ConsolaVideoJuego("Play Station 5","Sony","CFI-1100B", "Negra",300000,"")
-//PS5 MODELO CFI-11015B
-const consola1c = new ConsolaVideoJuego("Play Station 5", "Sony","CFI-11015B", "Blanca",350000,"")
-const consola1d = new ConsolaVideoJuego("Play Station 5", "Sony","CFI-11015B", "Negra",350000,"")
-//PS5 MODELO FFVII ESPECIAL EDITION
-const consola1e = new ConsolaVideoJuego("Play Station 5", "Sony","CFI-1100B", "FFVII ESPECIAL EDITION",500000,"")
+//PS4
+const consola1f = new Consola(playStation4,"slim", "blanco",200000,"")
+const consola1g = new Consola(playStation4,"pro", "negro",200000,"")
 
 
-//PS4 MODELO Slim 
-const consola1f = new ConsolaVideoJuego("Play Station 4", "Sony","Slim", "Blanca",200000,"")
-//PS4 MODELO PRO
-const consola1g = new ConsolaVideoJuego("Play Station 4", "Sony","PRO", "Negra",250000,"")
+//XBOX 
+const consola2a = new Consola(xboxSeries,"s", "negro",350000,"")
+const consola2b = new Consola(xboxSeries,"s", "blanco",350000,"")
 
-
-//XBOX----->
-
-//XBOX MODELO S
-const consola2a = new ConsolaVideoJuego("XBOX Series", "Microsoft","S", "Negra",350000,"")
-const consola2b = new ConsolaVideoJuego("XBOX Series", "Microsoft","S", "Blanca",350000,"")
 //XBOX MODELO X
-const consola2c = new ConsolaVideoJuego("XBOX Series", "Microsoft","X", "Negra",450000,"")
-const consola2d = new ConsolaVideoJuego("XBOX Series", "Microsoft","X", "Blanca",450000,"")
+const consola2c = new Consola(xboxSeries,"x", "negro",450000,"")
+const consola2d = new Consola(xboxSeries,"x", "blanco",450000,"")
 
 
 //XBOX ONE
-const consola2e = new ConsolaVideoJuego("XBOX ONE","Microsoft", "Classic", "Verde",200000,"")
-const consola2f = new ConsolaVideoJuego("XBOX ONE","Microsoft", "Classic", "Azul",200000,"")
-//XBOX ONE MODELO S
-const consola2g = new ConsolaVideoJuego("XBOX ONE","Microsoft", "S", "Roja",200000,"")
+const consola2e = new Consola(xboxOne,"classic", "verde",250000,"")
+const consola2f = new Consola(xboxOne,"classic",  "rojo",250000,"")
 
-
-//NINTENDO----->
 
 //Nintendo Oled
-const consola3a = new ConsolaVideoJuego("Nintendo Switch","Nintendo","Oled", "Blanca", 450000,"")
-const consola3b = new ConsolaVideoJuego("Nintendo Switch","Nintendo","Oled", "Negra", 450000,"")
+const consola3a = new Consola(nintendoSwitch,"oled", "negro",450000,"")
+const consola3b = new Consola(nintendoSwitch,"oled",  "blanco",450000,"")
 //Nintendo Switch classic
-const consola3c = new ConsolaVideoJuego("Nintendo Switch","Nintendo","Classic", "Blanca", 350000,"")
-const consola3d = new ConsolaVideoJuego("Nintendo Switch","Nintendo","Classic", "Azul", 350000,"")
+const consola3c = new Consola(nintendoSwitch,"classic",  "negro",380000,"")
+const consola3d = new Consola(nintendoSwitch,"classic",  "blanco",380000,"")
 
 //Nintendo DS XL
-const consola3e = new ConsolaVideoJuego("Nintendo DS","Nintendo","XL", "Silver", 150000,"")
+const consola3e = new Consola(nintendoDS,"ds xl", "azul",250000,"")
 //Nintendo DS
-const consola3f = new ConsolaVideoJuego("Nintendo DS","Nintendo","Classic", "Gold", 150000,"")
-*/
+const consola3f = new Consola(nintendoDS,"ds", "gold",180000,"")
+
 //<-----------------------------------------------Video Juegos--------------------------------------------------->
 
 //PELEA ----->
 
-const juego1 = new VideoJuego = ("accion","Mortal Combat X",consola1a, 30000,"")
-const juego2 = new VideoJuego = ("accion","Mortal Combat XI",consola1a, 35000,"")
-const juego3 = new VideoJuego = ("accion","",consola1a, 30000,"")
+const juego1 = new VideoJuego("accion","Mortal Combat X",playStation5, 30000,"")
+const juego2 = new VideoJuego("accion","Mortal Combat XI",playStation4, 35000,"")
+const juego3 = new VideoJuego("accion","Crisis 3",playStation4, 30000,"")
+const juego4 = new VideoJuego("Racing","Mario Kart",nintendoDS,40000,"")
 
 //<------------------------------------------------Arrays--------------------------------------------------------->
 
 
 //Array de consolas
-const consolas = [consola1a,consola1b,consola1c,consola1d,consola1e,consola1f,consola1g,consola2a,
-    consola2b,consola2c,consola2d,consola2e,consola2f,consola2g,consola3a,consola3b,consola3c,consola3d,
+const consolas = [consola1a,consola1b,consola1c,consola1d,consola1f,consola1g,consola2a,
+    consola2b,consola2c,consola2d,consola2e,consola2f,consola3a,consola3b,consola3c,consola3d,
     consola3e,consola3f]
 
+const carro = []
 
 //<--------------------------------------------------------------------------------------------------------->
 
 function mostrarConsolas(){
 
     consolas.forEach((consola)=>{
-        console.log(consola.marca)
+        console.log(consola.Fabricante.marca,consola.modelo,consola.color)
 
     })
 
 }
 
-const carro = [consola1a, consola1b]
+
 
 mostrarConsolas()
 
-function agregarAlCarroConDescuento(array, funcion){
-    for (const producto of array) {
-        funcion(producto)
-        console.log(array)
-    }
-
-}
-
-agregarAlCarroConDescuento(carro, () =>{ carro.push( 0.80)})
-
-function descuento (producto) {
-        carro.push(producto * 0.80)
-    
-}
 
 
 function comprarConsola(){
     
-    const seleccion = prompt("Que consola buscas? Nintendo, PlayStation XBOX").toLocaleLowerCase()
-    
-    switch(seleccion){
-        case "nintendo":
+    let seguirCompra = true
+
+    while ( seguirCompra == true) {
+        
+        const seleccion = prompt("Que consola buscas? Nintendo, PlayStation o XBOX").toLocaleLowerCase()
+
+        if(seleccion == "nintendo" || seleccion == "playstation" || seleccion == "xbox") {
             
-            consolas.forEach((consola) =>{
-                console.log(consola.nombre, consola.modelo, consola.color, consola.precio)
+            const busqueda = consolas.filter((consola) => consola.Fabricante.marca.includes(seleccion))
+                console.log(busqueda)
+            
+            const especificacion = prompt("Cual quieres llevar al carro?").toLocaleLowerCase()
+            
+            if(consolas.find((consola) => consola.modelo == especificacion)){
+                const agregar = consolas.find((consola) => consola.modelo.includes(especificacion))
+                carro.push(agregar)  
                 
-            }) 
+            }else{
+
+                alert("No tenemos que producto que indica")
+            }
+
+        }else{
+
+            alert("Debe indicar una marca existente")
+        }
         
-        case "playstation":
-            consolas.forEach((consola) =>{
-                console.log(consola.nombre, consola.modelo, consola.color, consola.precio)
-                
-            }) 
-
-        case "xbox":
-            consolas.forEach((consola) =>{
-                console.log(consola.nombre, consola.modelo, consola.color, consola.precio)
-                
-            }) 
-
-    }   
+        seguirCompra = confirm("Desea agregar algu producto adiconal?")
+    }  
         
-
-
-
 }
+
+comprarConsola()
+console.log(carro)
+
+let resultadoFuncion
+
+//recorreObjeto(consola3f,especificacion,resultadoFuncion)
+//buscandoElObjeto(consolas,resultadoFuncion)
+
+function buscandoElObjeto(array,variable){
+    array.forEach((objeto) => {
+        if(variable == array[objeto.modelo]){
+            variable = array[objeto]
+        }
+    })
+        
+}
+
+
+function recorreObjeto(objeto, variable, resultado){
+    for (const propiedad in objeto) {
+        if(variable == objeto[propiedad]){
+           resultado = objeto[propiedad]
+            
+        }
+        
+    }
+
+    console.log(resultado)
+} 
+       
+  
+
+
     
-
-
-
-
-/*
-const mostrarPorConsola = (Consola) =>{
-    console.log("El cuadro para " + cuadro.categoria + ", "+ cuadro.genero + ", tamaño " + cuadro.medida + 
-                ", de color "+ cuadro.color + ", tiene un precio de $" + cuadro.precio)
-
-}
-mostrarPorConsola(cuadro1)
-*/
