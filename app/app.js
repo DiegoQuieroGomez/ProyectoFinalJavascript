82ba7454059adc3b3058a99969e5a931c80d1264
Diegoquiero <61029293+Diegoquiero@users.noreply.github.com>
2022-06-27 22:42:28 -0400

    Fetch

--- app/app.js
+++ app/app.js
@@ -2,19 +2,9 @@
 
 //Declaraciones
 
-const consolas = [consola1a,consola1b,consola1c,consola1d,consola1f,consola1g,consola2a,consola2b,consola2e,
-    consola2f,consola3a,consola3b,consola3c,consola3d,consola3e,consola3f]
-
-const juegos = [juego1a,juego1b,juego1c,juego1d,juego1e,juego1f,juego1g,juego2a,juego2b,juego2c,juego2d,
-    juego2e,juego2f,juego2g,juego3a,juego3b,juego3c,juego3d,juego3e,juego3f,juego3g]
-
-const accesorios = []
-
-const otros = []
-
-const ofertas = [consola1a,consola1b,consola1c,consola1d]
-
 let carro = []
+
+let mix = []
 
 //Array Ultima Compra en Local Storage
 let compraPrevia = []
@@ -24,7 +14,6 @@ let guardarCompra = []
 
 const historialDeBusqueda = []
 
-const todosMisProductos = [...consolas,...juegos]
 
 
 //<--------------------------------------------------------------------------------------------------------->
@@ -43,129 +32,25 @@ const mostrarNombre = () =>{
     console.log("evento Escuchado")
 }
 
+//Aplicacion Fetch
 
-//Muestra ofertas
-function mostrarOfertas(){
-    ofertas.forEach((producto) => {
-        const card = document.createElement("div")
-        card.className = "card"
-        card.innerHTML = `
-            <h3 class="cardTitle">${producto.nombre}</h3>
-            <img src="${producto.imagen}" class="cardImg">
-            <p class="cardDesc"> Modelo ${producto.modelo} - Color ${producto.color}
-            <span class="cardPrice"> $${producto.precio*0.75}<BR>25% desc.</span>
-            <button data-id="${producto.Fabricante.marca}" class="buttonCTA"> Agregar al carro </button>
-            `
-        cardContainer.append(card)
-    })
-}
-
-//-----------------Lista productos dependiendo de la marca------------------------------------
-
-function listarProductosPorMarca(array){
-    productContainer.innerHTML =""
-    array.forEach((producto) => {
-        
-        const item = document.createElement("div")
-        item.className = "product"
-        item.innerHTML =`         
-            <img src="${producto.imagen}" class="productImg">
-            <h2 class="productName"> ${producto.tipoProducto} ${producto.nombre} </h2>
-            <span class="productPrice"> $ ${producto.precio} </span>
-            <button data-id="${producto.id}" class="buttonCTA"> Agregar al carro </button>
-         `
-        productContainer.append(item)
-        //Añade listeners al contenido generado dinámicamente
-        listenersContenidoGenerado(".buttonCTA", agregarProducto)
-       
-    })
+const productosApi = async (e) => {
+    const response = await fetch ("data/productos.json")
+    const data = await response.json()
+    mix = data
     
 }
 
-//<----------------------------------------Menu Desplegable-------------------------------------------------------->
+productosApi()  
 
-//Consolas
-const mostrarConsolas = (e) => {
+
+const renderizarProductos = (e) => {
+    const fabricanteElegido = e.target.getAttribute("id")
     const productosElegidos = e.target.getAttribute("data-id")
-    const productos = consolas.filter((productos) => productos.Fabricante.marca == productosElegidos)
-    listarProductosPorMarca(productos)
+    const productos = mix[fabricanteElegido].producto.filter((productos) => productos.tipoProducto == productosElegidos)
+    listarProductosPorTipo(productos)
 }
 
-//Juegos
-const mostrarJuegos = (e) => {
-    const productosElegidos = e.target.getAttribute("data-id")
-    const productos = juegos.filter((productos) => productos.Fabricante.marca == productosElegidos)
-    listarProductosPorMarca(productos)
-}
-
-//Accesorios
-const mostrarAccesorios = (e) => {
-    const productosElegidos = e.target.getAttribute("data-id")
-    const productos = todosMisProductos.filter((productos) => productos.Fabricante.marca == productosElegidos)
-    listarProductosPorMarca(productos)
-}
-
-//Otros
-const mostrarOtros = (e) => {
-    const productosElegidos = e.target.getAttribute("data-id")
-    const productos = todosMisProductos.filter((productos) => productos.Fabricante.marca == productosElegidos)
-    listarProductosPorMarca(productos)
-}
-
-//---------------------------------------------------------------------------------------------------------------------------------------------------
-
-//Añadir al Carro
-const agregarProducto = (e) =>{
-    const productoElegido = e.target.getAttribute("data-id")
-    const producto = todosMisProductos.find((prod) => prod.id == productoElegido)
-    carro.push(producto)
-    guardarCompra = JSON.stringify(carro)
-    localStorage.setItem("guardarCompra", guardarCompra)
-    console.log(producto)
-}
-
-
-
-//<--------------------------------Listeners----------------------------------------->
-
-historialDeCompra.addEventListener("click",ultimaCompra)
-botCarro.addEventListener("click", listarCarro)
-
-
-//INTENTO DE CONFIRMACION ANTES DE ACTUALIZAR O SALIR DEL NAVEGADOR*****************
-/*
-window.addEventListener("onbeforeunload", (e)=>{
-    e.preventDefault()
-
-    confirm("seguro deseas salir?")
-
-   
-    Swal.fire({
-        title: 'Do you want to save the changes?',
-        showDenyButton: true,
-        showCancelButton: true,
-        confirmButtonText: 'Save',
-        denyButtonText: `Don't save`,
-      }).then((result) => {
-        // Read more about isConfirmed, isDenied below //
-        if (result.isConfirmed) {
-          
-        } else if (result.isDenied) {
-          
-        }
-      })
-    
-})
-*/
-
-//Añade Listeners al contenido generado dinámicamente
-function listenersContenidoGenerado(clase, funcion){
-    const botonesCompra = document.querySelectorAll(clase)
-    botonesCompra.forEach((botonCompra) => {
-        botonCompra.addEventListener("click", funcion)
-    })
-
-}
 
 //Listeners al Menú
 function listenerMenu(clase,funcion){
@@ -176,10 +61,72 @@ function listenerMenu(clase,funcion){
     
 }
 //Ejecución Menú
-listenerMenu(".listaConsola",mostrarConsolas)
-listenerMenu(".listaJuego",mostrarJuegos)
-listenerMenu(".listaAccesorio",mostrarAccesorios)
-listenerMenu(".listaOtro",mostrarOtros)
+listenerMenu(".listaConsola",renderizarProductos)
+
+
+//-----------------Lista productos dependiendo de la marca------------------------------------  ARREGLAR
+
+function listarProductosPorTipo(array){
+    productContainer.innerHTML =""
+    array.forEach((producto) => {
+        
+        const item = document.createElement("div")
+        item.className = "product"
+        item.innerHTML =`         
+            <img src="${producto.imagen}" class="productImg">
+            <h2 class="productName"> ${producto.tipoProducto} ${producto.nombre} </h2>
+            <span class="productPrice"> $ ${producto.precio} </span>
+            <button id= "${producto.sku}" class="buttonCTA"> Agregar al carro </button>
+         `
+        productContainer.append(item)
+        //Añade listeners al contenido generado dinámicamente
+        listenersContenidoGenerado(".buttonCTA", agregarProducto)
+
+    })
+    
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Añadir al Carro
+const agregarProducto = (e) =>{
+   
+    const productoElegido = e.target.getAttribute("id")
+    mix[0].producto.find((producto) => producto.sku == productoElegido)
+    
+    
+
+    
+     
+    console.log(producto)
+    console.log(fabricante)
+ 
+    
+   
+   
+    //const producto = mix.find((producto) => producto.sku == productoElegido)
+    //carro.push(producto)
+
+    guardarCompra = JSON.stringify(carro)
+    localStorage.setItem("guardarCompra", guardarCompra)
+   
+}
+
+
+//<--------------------------------Listeners----------------------------------------->
+
+historialDeCompra.addEventListener("click",ultimaCompra)
+botCarro.addEventListener("click", listarCarro)
+
+
+//Añade Listeners al contenido generado dinámicamente
+function listenersContenidoGenerado(clase, funcion){
+    const botonesCompra = document.querySelectorAll(clase)
+    botonesCompra.forEach((botonCompra) => {
+        botonCompra.addEventListener("click", funcion)
+    })
+
+}
 
 
 //Verificar Ultima Compra
